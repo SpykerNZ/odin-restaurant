db9e92753f3acd9f20f11bca1475a62701d1c316
SpykerNZ <jrspyker@gmail.com>
2023-01-16 20:32:21 +1300

    improve page titles

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,14 +2,26 @@ export default function createContactPage() {
   const contactElems = document.createElement("div");
   contactElems.classList.add("contact");
 
+  // Add Title Elem
+  const titleElem = document.createElement("p");
+  titleElem.textContent = "Contact Us";
+  titleElem.classList.add("title");
+  contactElems.append(titleElem);
+
+  // Add Contact Items
+  const contactItemsElem = document.createElement("div");
+  contactItemsElem.classList.add("items");
+
   const jsonContactData = require("../data/contact.json");
   console.log(jsonContactData);
   for (let key in jsonContactData) {
     const elem = document.createElement("p");
     elem.classList.add(key);
     elem.textContent = jsonContactData[key];
-    contactElems.append(elem);
+    contactItemsElem.append(elem);
   }
+
+  contactElems.append(contactItemsElem);
 
   return contactElems;
 }
